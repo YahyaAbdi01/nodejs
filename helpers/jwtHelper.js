3d56dab2ef1b446df2143423af65a22ce628c28a
Yahya <yeahyes766@gmail.com>
2024-07-03 13:00:48 +0300

    2nd

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -30,7 +30,7 @@ signAccessToken: (UserId)=> {
 verifyAccessToken:(req, res, next)=>{
     if(!req.headers['authorization']) return next(createError.Unauthorized());
     const authHeader = req.headers['authorization'];
-    const bearerToken = authHeader.split(' ');
+    const bearerToken = authHeader.split('');
     const token = bearerToken[1];
     JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload)=>{
         if(err){
